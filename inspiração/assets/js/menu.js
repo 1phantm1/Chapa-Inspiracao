072399c59a3475355983c223a8e6fefa32ca0f1c
1phantm1 <pietrodavila37@gmail.com>
2024-05-18 15:07:59 -0300

    first

---
+++ inspiração/assets/js/menu.js
@@ -0,0 +1,26 @@
+const burguer_icon = document.querySelector("i#burguer-icon")
+const closed = document.querySelector("i#close")
+const menu = document.querySelector("ul#menu_list")
+const overlay = document.querySelector("div#overlay")
+let isopen = false
+
+burguer_icon.addEventListener("click", function burguer() {
+  if (!isopen) {
+    closed.style.display = "block"
+    menu.classList.toggle("open")
+    
+    overlay.style.display = "block"
+  } else {
+    closed.style.display = "none"
+  }
+  isopen = !isopen
+})
+
+closed.addEventListener("click", () => {
+  if (isopen) {
+    closed.style.display = "none"
+    overlay.style.display = "none"
+    menu.classList.remove("open")
+  }
+  isopen = !isopen
+})
