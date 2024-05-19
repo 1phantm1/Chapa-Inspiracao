b1cada4e3369cca7da96426677618acac92ca84b
1phantm1 <pietrodavila37@gmail.com>
2024-05-19 18:23:34 -0300

    save

--- inspiração/assets/js/smooth_scroll.js
+++ inspiração/assets/js/smooth_scroll.js
@@ -43,69 +43,68 @@ ScrollReveal().reveal('.propostas_resumo p', {
 });
 ScrollReveal().reveal('.proposta_block', { 
     reset: true,
-    duration: 1000,
     easing: "ease-out",
     origin: 'bottom', 
     distance: '50px', 
-    duration: 1000, 
-    interval: 200,
+    duration: 500,
+    delay: 100,
 });
 ScrollReveal().reveal(".who_are_we h2", {
     reset: true,
     origin: "top",
     distance: "70px",
-    duration: 1000,
-    delay: 500
+    duration: 500,
+    delay: 100,
 })
 ScrollReveal().reveal(".who_are_we h3", {
     reset: true,
     origin: "right",
     distance: "500px",
-    duration: 1000,
-    delay: 500
+    duration: 500,
+    delay: 100,
 })
 ScrollReveal().reveal(".who_are_we h4", {
     reset: true,
     origin: "bottom",
     distance: "150px",
-    duration: 1000,
-    delay: 500
+    duration: 500,
+    delay: 100,
 })
 ScrollReveal().reveal(".who_are_we p", {
     reset: true,
     origin: "left",
     distance: "250px",
-    duration: 1000,
-    delay: 500
+    duration: 500,
+    delay: 100,
 })
 
 ScrollReveal().reveal(".who_are_we li", {
     reset: true,
     origin: "right",
     distance: "350px",
-    duration: 1000,
-    delay: 500
+    duration: 500,
+    delay: 100,
 })
 ScrollReveal().reveal(".members h3", {
     reset: true,
     origin: "bottom",
     distance: "500px",
-    duration: 1000,
-    delay: 500,
+    duration: 500,
+    delay: 100,
 })
 
 ScrollReveal().reveal(".members .card", {
     reset: true,
     origin: "left",
     distance: "500px",
-    duration: 1000,
-    delay: 500,
+    duration: 500,
+    delay: 100,
 })
 ScrollReveal().reveal(".members p", {
     reset: true,
     origin: "right",
     distance: "500px",
-    duration: 1000,
-    delay: 500,
+    duration: 500,
+    delay: 100,
 })
 
